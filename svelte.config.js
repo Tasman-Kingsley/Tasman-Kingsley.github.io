ee10986e4b76173bd7178bd653dc91fdba53ee55
tasman-kingsley <tasman.kingsley@gmail.com>
2021-08-03 21:27:12 +1000

    svelte-build-0.1

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,14 @@
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
-};
+// svelte.config.js
+// eslint-disable-next-line import/no-extraneous-dependencies
+import adapter from '@sveltejs/adapter-static';
 
-export default config;
+export default {
+  kit: {
+    adapter: adapter({
+      // default options are shown
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+    }),
+  },
+};
