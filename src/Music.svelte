62e2dabf7ce065c980f1237e8118430bbc3044a3
tasman-kingsley <tasman.kingsley@gmail.com>
2021-08-04 09:43:55 +1000

    build

---
+++ src/Music.svelte
@@ -0,0 +1,8 @@
+<div class="content subDiv">
+    <div id="contentDiv">
+      <img id="profile" src="images/SpotifyProfile.png" alt="">
+      <p>Artist - Tasman Kingsley</p>
+      <iframe src="https://open.spotify.com/embed/artist/761DscW9vfSQMmnKEJNHbH"
+      width="500" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+    </div>
+</div>
