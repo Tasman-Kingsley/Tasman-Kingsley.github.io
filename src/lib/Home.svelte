a5a6741aee4c065d468945a1fe17ce53b3f5168b
tasmankingsley <tasman.kingsley@gmail.com>
2022-03-19 19:41:50 +1100

    build

--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -8,7 +8,7 @@
 
     <!-- <iframe src="https://tasmankingsley.github.io/tetris/"
     width="380" height="630" style=""></iframe> -->
-    <iframe src="https://tasmankingsley.github.io/cube-three/" width="400" height="400"></iframe>
+    <!-- <iframe src="https://tasmankingsley.github.io/cube-three/" width="400" height="400"></iframe> -->
 </div>
 
 <style>
