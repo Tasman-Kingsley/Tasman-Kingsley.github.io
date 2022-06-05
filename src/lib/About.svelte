6592028891b654438f2bd882abfd68c0a70e8061
tasmankingsley <tasman.kingsley@gmail.com>
2022-06-05 23:06:19 +0900

    content update

--- src/lib/About.svelte
+++ src/lib/About.svelte
@@ -6,10 +6,10 @@
     <img src="./images/choco-profile.jpg" alt="">
     <span style="color: #b2c6d3; padding: 20px;">About me</span>
     <p>Hello fellow human! My name is Tasman. <br><br>
-    I am an amateur Designer, Developer, and Music Producer.<br><br>
+    I am an amateur Interaction Designer, Software Developer, and Music Producer.<br><br>
     Thanks for finding your way to my portfolio page. Be sure to poke around and look at my projects if you'd like.<br><br>
     Also, check out my music if you find yourself curious, i appreciate the time and curiosity.<br><br>
-    You can contact me at <a href="mailto: tasman.kingsley@gmail.com" style="color: #d3c7b8;">tasman.kingsley@gmail.com</a></p>
+    You can contact me at <a href="mailto: tasmankingsley@gmail.com" style="color: #d3c7b8;">tasman.kingsley@gmail.com</a></p>
 </div>
 
 <style>
