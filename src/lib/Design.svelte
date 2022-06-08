edcc48a6b428166e9f5e101a227f057c88cc7843
tasmankingsley <tasman.kingsley@gmail.com>
2022-06-08 20:16:18 +0900

    update

--- src/lib/Design.svelte
+++ src/lib/Design.svelte
@@ -34,9 +34,6 @@
             laudantium maiores, dolore maxime iusto molestias error vel velit magnam tempora 
             distinctio aut amet fugiat nihil doloribus tenetur temporibus!</p>
     
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid esse eligendi 
-            laudantium maiores, dolore maxime iusto molestias error vel velit magnam tempora 
-            distinctio aut amet fugiat nihil doloribus tenetur temporibus!</p><br>
     </div>
 </div>
 
